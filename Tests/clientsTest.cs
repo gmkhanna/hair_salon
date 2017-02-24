b298ce4baad41df2de5e21adba8d787e047fab9c
Grinil Khanna <gmkhanna@gmail.com>
2017-02-24 11:10:55 -0800

    Add test to make sure save function is working.

--- Tests/clientsTest.cs
+++ Tests/clientsTest.cs
@@ -24,11 +24,57 @@ namespace HairSalonApp
         }
 
         [Fact]
+        public void Test_OverrideTrueIfSameName()
+        {
+            //Arrange
+            Client userInput1 = new Client("Client Name", 1);
+            Client userInput2 = new Client("Client Name", 1);
+
+            //Act
+            testInput = userInput1.Save();
+            result = userInput2.Save();
+
+            //Assert
+            Assert.Equal(result, testInput);
+        }
+
+        // [Fact]
+        // public void Test_OverrideTrueIfSameName()
+        // {
+        //     //Arrange
+        //     Client userInput1 = new Client("Client Name", 1);
+        //     Client userInput2 = new Client("Client Name", 1);
+        //
+        //     //Act
+        //     userInput1.Save();
+        //     userInput2.Save();
+        //
+        //     //Assert
+        //     Assert.Equal(userInput2, userInput1);
+        // }
+
+        [Fact]
+        public void Test_SaveObjectToDB()
+        {
+            //Arrange
+            Client userInput = new Client("Client Name", 1);
+            userInput.Save();
+
+            //Act
+            List<Client> testInput = new List<Client>{userinput};
+            List<Client> result = Client.GetAll();
+
+            //Assert
+            Assert.Equal(result, testInput);
+        }
+
+
+        [Fact]
         public void Test_SaveAssignstoID()
         {
             //Arrange
             Client userInput = new Client("Client Name", 1);
-            userInput.Save()
+            userInput.Save();
             Client savedInput = Client.GetAll()[0];
 
             //Act
